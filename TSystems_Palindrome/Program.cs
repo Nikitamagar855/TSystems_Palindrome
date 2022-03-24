bcd36631ca86a2bde14f304499d4bcade6ffb475
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-24 11:47:53 +0530

    Add project files.

---
+++ TSystems_Palindrome/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            Console.WriteLine("ENTER ANY NUMBER");
+            int NUM = Convert.ToInt32(Console.ReadLine());
+            int temp, r, sum = 0;
+            temp = NUM;
+            while (NUM > 0)
+            {
+                r = NUM % 10;
+                sum = (sum * 10) + r;
+                NUM = NUM / 10;
+
+            }
+            if (temp == sum)
+            {
+                Console.WriteLine("NUMBER IS PALINDROME");
+            }
+            else
+            {
+                Console.WriteLine("NOT PALINDROME");
+            }
+            Console.ReadLine();
+            goto start;
+
+        }
+    }
+}
